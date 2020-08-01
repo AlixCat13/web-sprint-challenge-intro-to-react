6d55bdecffd20573d0b35bda19e8460a8fff8d74
Cody C <alix.henry@ymail.com>
2020-07-31 21:49:24 -0500

    finally got the project up in the dev browser but now I can't get the API request to work

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+export default function Character() {
+  
+    useEffect(() => {
+       axios.get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+            console.log(response)
+          })
+         .catch(error => console.log(error))
+    }, [])
+
+
+    return (
+        <div className='RnM' >
+            
+        </div>
+    )
+    
+
+
+
+
+
+}
