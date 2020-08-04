bc8a9b35645eede4a208f9dbf36040b3fc8869f3
Cody C <alix.henry@ymail.com>
2020-08-03 22:04:42 -0500

    Got the styled components to work finally

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,16 +1,26 @@
    import React from 'react'
+   import styled from 'styled-components'
+
+  const CharacText = styled.p `
+   color: black; 
+   font-weight: bold;  
+   `;
+
+   const Photo = styled.img `
+   margin-top: 2%;
+   `;
    
    const CharacterCard = ({ data }) => {
      return (
        
       <div>
         <div className='characters'>
-       <img className='photo' alt='Character' src= {data.image} />
-             <p className='name'>Name: {data.name}</p>
-          <p className='gender'>Gender: {data.gender}</p>
-         <p className='location'>Location: {data.location.name}</p>
-       <p class='species'>Species: {data.species}</p>
-     <p className='status'>Status: {data.status}</p>
+       <Photo className='photo' alt='Character' src= {data.image} />
+             <CharacText className='name'>Name: {data.name}</CharacText>
+          <CharacText className='gender'>Gender: {data.gender}</CharacText>
+         <CharacText className='location'>Location: {data.location.name}</CharacText>
+       <CharacText class='species'>Species: {data.species}</CharacText>
+     <CharacText className='status'>Status: {data.status}</CharacText>
      </div> 
      </div>    
      )   
